f7e56705335d29e3af8ea57c5c6a12756de71357
hidaya <hidaya.regragui@gmail.com>
2024-11-04 20:40:25 +0100

    task5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,6 +40,37 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(result, expected_url)
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test that GithubOrgClient.public_repos
+        returns the expected list of repos"""
+        mock_get_json.return_value = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"}
+        ]
+
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_repos_url:
+            mock_repos_url.return_value = (
+                "https://api.github.com/orgs/test_org/repos"
+            )
+
+            client = GithubOrgClient("test_org")
+
+            result = client.public_repos()
+            expected_repos = ["repo1", "repo2", "repo3"]
+
+            self.assertEqual(result, expected_repos)
+
+            mock_repos_url.assert_called_once()
+
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/test_org/repos"
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
