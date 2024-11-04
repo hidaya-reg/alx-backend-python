e0133bb53d3d12dc12bdf77c9884550dd099bb8a
hidaya <hidaya.regragui@gmail.com>
2024-11-04 18:43:38 +0100

    task2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """Parameterize a unit test"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,6 +26,30 @@ class TestAccessNestedMap(unittest.TestCase):
         """test that a KeyError is raised"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test cases for get_json"""
+
+    @patch('requests.get')
+    def test_get_json(self, mock_get):
+        """test that utils.get_json returns the expected result."""
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
+            mock_get.reset_mock()
 
 
 if __name__ == '__main__':
