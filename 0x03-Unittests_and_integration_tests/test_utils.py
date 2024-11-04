1f13c0227e761d33789470c1a9526426ce147cc9
hidaya <hidaya.regragui@gmail.com>
2024-11-04 17:51:01 +0100

    task1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test that the method returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """test that a KeyError is raised"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 
 if __name__ == '__main__':
